4921bde64db8469ae3d943f407065c4925ffd6dc
Elmar Putz <ep@studiomitte.com>
2021-02-25 15:15:05 +0100

    header & footer added

--- index.php
+++ index.php
@@ -5,5 +5,10 @@ $default_view = 'welcome';
 
 $view = $default_view;
 
+if (isset($_REQUEST['view']) && 
+    file_exists(__DIR__.'/views/' . $_REQUEST['view'] . '.php')) {
+        $view = $_REQUEST['view'];
+    }
+
 
 require_once('views/' . $view . '.php');
