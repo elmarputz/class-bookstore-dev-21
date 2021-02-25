c77a5c6f8af92d54e2a683ecedab9ab542641b65
Elmar Putz <ep@studiomitte.com>
2021-02-25 14:57:15 +0100

    welcome view

---
+++ inc/bootstrap.php
@@ -0,0 +1,3 @@
+<?php 
+declare(strict_types=1);
+error_reporting(E_ALL); ini_set('display_erros', 'On');
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-<?php phpinfo(); 
-/* test commit lesson-01 */
+<?php
+require_once('inc/bootstrap.php');
+
+$default_view = 'welcome';
+
+$view = $default_view;
+
+
+require_once('views/' . $view . '.php');
