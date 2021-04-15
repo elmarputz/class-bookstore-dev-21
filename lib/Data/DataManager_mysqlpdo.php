1151cd7d4ad8557310e3733e9a7efee96a33cd7f
Elmar Putz <ep@studiomitte.com>
2021-04-15 17:49:19 +0200

    add user

--- lib/Data/DataManager_mysqlpdo.php
+++ lib/Data/DataManager_mysqlpdo.php
@@ -30,6 +30,31 @@ class DataManager implements IDataManager {
     }
 
 
+    private static function query ($connection, $query, $parameters = []) {
+        $connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION); 
+        try {
+            $statement = $connection->prepare($query);
+            $i = 1;
+            foreach ($parameters as $param) {
+                if (is_int($param)) {
+                    $statement->bindValue($i, $param, \PDO::PARAM_INT); 
+                }
+                if (is_string($param)) {
+                    $statement->bindValue($i, $param, \PDO::PARAM_STR);
+                }
+                $i++;
+            }
+            $statement->execute();
+
+
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+        return $statement; 
+
+    }
+
+
 
     private static function closeConnection() {
         self::$__connection = null;
@@ -63,17 +88,62 @@ class DataManager implements IDataManager {
     }
 
     public static function getBooksByCategory(int $categoryId) : array {
-        $res = [];
-        
-        return $res;
+        $books = [];
+        $con = self::getConnection();
+        $res = self::query($con, "
+            SELECT id, categoryId, title, author, price  
+            FROM books
+            WHERE categoryId = ?;",
+            [$categoryId]
+        );
+
+        while ($book = self::fetchObject($res)) {
+            $books[] = new Book($book->id, $book->categoryId, $book->title, 
+                $book->author, $book->price);
+        }
+
+        self::close($res);
+        self::closeConnection();
+        return $books;
     }
 
     public static function getUserById (int $userId) : ?User {
-      return null;
+      $user = null;
+      $con = self::getConnection();
+      $res = self::query($con, "
+          SELECT id, userName, passwordHash  
+          FROM users
+          WHERE id = ?;",
+          [$userId]
+      );
+
+      if ($u = self::fetchObject($res)) {
+          $user = new User($u->id, $u->userName, $u->passwordHash);
+      }
+
+      self::close($res);
+      self::closeConnection();
+      return $user;
+
     }
 
     public static function getUserByUserName (string $userName) : ?User {
-      return null;
+        $user = null;
+        $con = self::getConnection();
+        $res = self::query($con, "
+            SELECT id, userName, passwordHash  
+            FROM users
+            WHERE userName = ?;",
+            [$userName]
+        );
+  
+        if ($u = self::fetchObject($res)) {
+            $user = new User($u->id, $u->userName, $u->passwordHash);
+        }
+  
+        self::close($res);
+        self::closeConnection();
+        return $user;
     }
 
 
